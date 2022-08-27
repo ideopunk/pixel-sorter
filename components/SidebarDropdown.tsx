95d4c99e0a21e5e224ae5d9b6dcb1707057b8976
Conor Barnes <conorbarnes93@gmail.com>
2022-08-27 17:32:26 -0300

    refine dropdown spacing

--- components/SidebarDropdown.tsx
+++ components/SidebarDropdown.tsx
@@ -28,10 +28,10 @@ export default function Dropdown<T>({
 				onClick={toggle}
 				className="flex hover:bg-slate-200 w-full justify-between items-center p-4 rounded-full"
 			>
-				<p className="text-sm tracking-wide">{title.toUpperCase()}</p>
+				<p className="text-sm">{title.toUpperCase()}</p>
 				<div className="flex items-center">
 					<p className="font-bold">{(value as unknown as string).toUpperCase()}</p>
-					<div className={`ml-2 scale-90  ${thisCurrent ? "rotate-180" : "rotate-0"}`}>
+					<div className={`ml-1 scale-90  ${thisCurrent ? "rotate-180" : "rotate-0"}`}>
 						<Chevron />
 					</div>
 				</div>
